0802c3e9d7ef7a666547a198eca97b81df08c17f
MasoudAndiwal <masoudandiwal89@gmail.com>
2025-04-26 17:10:13 +0430

    add manifest.json for create the web app

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo192.png',
+  '/logo512.png',
+  '/src/main.jsx',
+  '/src/App.jsx',
+  '/src/index.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+}); 
